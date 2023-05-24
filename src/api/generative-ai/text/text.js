f1126b2ac32bc4ce91c1e98a98dbd250edf01ae1
greendream21 <uri.dev.0802@gmail.com>
2023-05-24 15:14:48 +0530

    Implemented Text - Anthropic

--- src/api/generative-ai/text/text.js
+++ src/api/generative-ai/text/text.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const   {OpenAi}  = require("../../../controllers/generative-ai/text/textController");
+const   {OpenAi , Anthropic}  = require("../../../controllers/generative-ai/text/textController");
 const  asyncHandler  =  require("../../../middlewares/asyncErrorHandler");
 
-router.post("/text/openai", asyncHandler(OpenAi)) 
+router.post("/text/openai", asyncHandler(OpenAi))
+router.post("/text/anthropic", asyncHandler(Anthropic)) 
 
 module.exports =  router;
--- src/controllers/generative-ai/text/textController.js
+++ src/controllers/generative-ai/text/textController.js
@@ -2,7 +2,54 @@ require("dotenv").config();
 const axios = require("axios");
 
 
-
+//Implementaions:
+// 1. OpenAi- [Chatgpt(gpt-3.5-turbo) ,  gpt3(davinci)] - Completed
+// 2. Anthropic- [Claude-v1 , Claude-instant-v1.1] - Completed
+// 3. Cohere- [To be implemented] - Not Complete (need access)
+// 4. Bard- [To be implemented] - Not Complete (not available)
+exports.Anthropic = async (req, res) => {
+  try {
+    let prompt = req.body.prompt; //required
+    let model = req.body.model || "claude-v1"; //required {2 types: "claude-v1" and "claude-instant-v1.1" only} defaults to "claude-v1"
+    let max_tokens = req.body.max_tokens || 1000; //optional -defaults to 1000
+    let temperature = req.body.temperature || 0.7; //optional -defaults to 0.7
+    let data = JSON.stringify({
+      prompt: prompt,
+      model: model,
+      max_tokens_to_sample: max_tokens,
+      temperature: temperature,
+    });
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://api.anthropic.com//v1/complete",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": process.env.ANTHROPIC_API_KEY,
+      },
+      data: data,
+    };
+    axios
+      .request(config)
+      .then((response) => {
+        try {
+          res.send(response.data.completion);
+        } catch {
+          res.send("Error in getting response || Axios Error");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.error({
+      title: "Anthropic Text",
+      message: error.message,
+      date: new Date(),
+    });
+    return res.status(500).send("Server Error in Anthropic Text.");
+  }
+};
 
 exports.OpenAi = async (req, res) => {
   try {
